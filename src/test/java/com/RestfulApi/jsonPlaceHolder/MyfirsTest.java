cfdcc268344ec37564116b8647341057079d2367
rasidb <rasid5888@gmail.com>
2023-11-22 16:07:00 +0300

    MyfirsTest.java postMethod testi body asserti icin Map classına çevirildi pom.xml gson bağımlılığı eklendi

--- src/test/java/com/RestfulApi/jsonPlaceHolder/MyfirsTest.java
+++ src/test/java/com/RestfulApi/jsonPlaceHolder/MyfirsTest.java
@@ -3,6 +3,7 @@ package com.RestfulApi.jsonPlaceHolder;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -27,49 +28,43 @@ class MyfirsTest {
         //@ValueSource içinde bulunan obje sayısı kadar testlerimiz yapılacak
     void getMethod(int num) {  //int num objesi valueSource anotasyonu içinde yazdığımız objeleri işaret eder kaçıncı testte olduğumuza göre int değeri değişir
         Response response =// response objesi rest-assured classından geliyor
-            given()
-                    .accept(ContentType.JSON) //response body'nin hangi formatta olacağını belirliyoruz
-                    .log().uri() //request url görmemize yarar
-                    .pathParam("id", num) //id isimli bir path param oluşturur get methoduyla birlikte kullanılırsa request url'ye eklenir
-            .when()
-            .get("posts/{id}")// GET methodu göndereceğimizi ve id pathini kullanacağımızı belirttik bu methoddan sonra request gönderilir.
-            .then()
-                    .statusCode(200) //status code'nin 200 olduğunu doğrula
-                    .contentType(ContentType.JSON) //response body'nin json formatında olduğunu doğrula
-                    .body("id", is(num)) //body içinde bulunan id key'ine karşılık value'nin num olduğunu assert et (is() methodu matchers classından geliyor)
-                    .extract()
-                    .response();
+                given()
+                        .accept(ContentType.JSON) //response body'nin hangi formatta olacağını belirliyoruz
+                        .log().uri() //request url görmemize yarar
+                        .pathParam("id", num) //id isimli bir path param oluşturur get methoduyla birlikte kullanılırsa request url'ye eklenir
+                        .when()
+                        .get("posts/{id}")// GET methodu göndereceğimizi ve id pathini kullanacağımızı belirttik bu methoddan sonra request gönderilir.
+                        .then()
+                        .statusCode(200) //status code'nin 200 olduğunu doğrula
+                        .contentType(ContentType.JSON) //response body'nin json formatında olduğunu doğrula
+                        .body("id", is(num)) //body içinde bulunan id key'ine karşılık value'nin num olduğunu assert et (is() methodu matchers classından geliyor)
+                        .extract()
+                        .response();
         response.prettyPrint(); //response body'nin çıktısını al
     }
 
-    /**
-     *
-     * @param title
-     * @param body
-     * @param userId
-     */
     @DisplayName("example post method using csvFileSource annotation")
     @ParameterizedTest()
-    @CsvFileSource(resources = "/jsonPlaceHolder/jsonPlaceHolderBody.csv", numLinesToSkip = 1) //hazır csv dosyası kullanarak testi yap ilk satırı atla
+    @CsvFileSource(resources = "/jsonPlaceHolder/jsonPlaceHolderBody.csv", numLinesToSkip = 1)//hazır csv dosyası kullanarak testi yap ilk satırı atla
     void postMethod(String title, String body, int userId) {
         Map<String, Object> reqBody = new HashMap<>(); //request body için Map classından obje oluşturduk hata almamak için jackson bağımlılığını kurmak lazım
         reqBody.put("title", title);
         reqBody.put("body", body);
         reqBody.put("userId", userId); //request body oluşturuldu
-        given()
+        Map response = given()
                 .accept("application/json") //response json formatında olsun
                 .contentType(ContentType.JSON) //request body'nin formati json
                 .body(reqBody) //request body'miz
-        .when()
+                .when()
                 .post("/posts")//body içerdiği için post methodunu kullandık
-        .then()
+                .then()
                 .statusCode(201)//201 status code başarıyla oluşturuldu
                 .body("title", is(title))//body'nin title keyi ile csv dosyasındaki title'in aynı olduğunu doğrula
                 .body("body", is(body))//body'nin body keyi le csv dosyasının body'sinin aynı olduğunu doğrula
                 .body("userId", is(userId))//body'nin userId keyi ile csv dosyasının userId'sinin aynı olduğunu doğrula
-        .extract()
-                .response()//response objesine çevir
-                .prettyPrint();//konsola response yazdır
+                .extract()
+                .as(Map.class);
+        response.remove("id");
+        Assertions.assertEquals(reqBody,response);
     }
-
 }
